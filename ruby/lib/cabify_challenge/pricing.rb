ee845d8bd9c94d9afd9ce92a72555fb40dae3a03
Alvaro Polo <apoloval@gmail.com>
2017-06-07 19:25:13 +0200

    [Ruby] Introduce the `BulkDiscountPrice` pricing policy

--- ruby/lib/cabify_challenge/pricing.rb
+++ ruby/lib/cabify_challenge/pricing.rb
@@ -38,5 +38,32 @@ module CabifyChallenge
         @price_per_unit * (units - units.to_i / 2)
       end
     end
+
+    # A price policy that applies a discount for bulk purchases.
+    class BulkDiscountPrice
+
+      # Initialize this price with the given price per unit.
+      #
+      # @param price_per_unit [Float] The price per unit if bulk minimum is not reached
+      # @param discount_price_per_unit [Float] The price per unit if bulk minimum is reached
+      # @param bulk_minimum_units [Integer] The minimum units to consider a bulk purchase
+      def initialize(price_per_unit, discount_price_per_unit, bulk_minimum_units)
+        @price_per_unit = price_per_unit
+        @discount_price_per_unit = discount_price_per_unit
+        @bulk_minimum_units = bulk_minimum_units
+      end
+
+      # Calculate the price for the given units
+      #
+      # @param units [Integer]
+      # @return Float
+      def calculate(units)
+        if units < @bulk_minimum_units
+          @price_per_unit * units
+        else
+          @discount_price_per_unit * units
+        end
+      end
+    end
   end
 end
--- ruby/test/cabify_challenge/pricing_test.rb
+++ ruby/test/cabify_challenge/pricing_test.rb
@@ -23,5 +23,15 @@ module CabifyChallenge
       assert_equal 3.90, price.calculate(3)
       assert_equal 3.90, price.calculate(4)
     end
+
+    def test_bulk_discount_price
+      price = Pricing::BulkDiscountPrice.new(1.95, 1.75, 3)
+
+      assert_equal 0.0, price.calculate(0)
+      assert_equal 1.95, price.calculate(1)
+      assert_equal 3.90, price.calculate(2)
+      assert_equal 5.25, price.calculate(3)
+      assert_equal 7.0, price.calculate(4)
+    end
   end
 end
