7892231583e6bdad422298d636eb23229b0ba253
Alvaro Polo <apoloval@gmail.com>
2017-06-07 19:16:14 +0200

    [Ruby] Introduce the `GetTwoPayOnePrice` pricing policy

--- ruby/lib/cabify_challenge/pricing.rb
+++ ruby/lib/cabify_challenge/pricing.rb
@@ -19,5 +19,24 @@ module CabifyChallenge
         @price_per_unit * units
       end
     end
+
+    # A price policy that applies a discount of 2-for-1.
+    class GetTwoPayOnePrice
+
+      # Initialize this price with the given price per unit.
+      #
+      # @param price_per_unit [Float]
+      def initialize(price_per_unit)
+        @price_per_unit = price_per_unit
+      end
+
+      # Calculate the price for the given units
+      #
+      # @param units [Integer]
+      # @return Float
+      def calculate(units)
+        @price_per_unit * (units - units.to_i / 2)
+      end
+    end
   end
 end
--- ruby/test/cabify_challenge/pricing_test.rb
+++ ruby/test/cabify_challenge/pricing_test.rb
@@ -13,5 +13,15 @@ module CabifyChallenge
       assert_equal 1.95, price.calculate(1)
       assert_equal 3.90, price.calculate(2)
     end
+
+    def test_get_two_pay_one_price
+      price = Pricing::GetTwoPayOnePrice.new(1.95)
+
+      assert_equal 0.0, price.calculate(0)
+      assert_equal 1.95, price.calculate(1)
+      assert_equal 1.95, price.calculate(2)
+      assert_equal 3.90, price.calculate(3)
+      assert_equal 3.90, price.calculate(4)
+    end
   end
 end
