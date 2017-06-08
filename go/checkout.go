e456c30e58f65ac406b1ef23be9b35fd979ca10b
Alvaro Polo <apoloval@gmail.com>
2017-06-08 11:32:07 +0200

    [Go] Introduce the `Checkout` type

---
+++ go/checkout.go
@@ -0,0 +1,59 @@
+package cabify_challenge
+
+type PricingRules map[string]PriceCalculator
+
+func DefaultPrices() PricingRules {
+  return PricingRules {
+    "VOUCHER": GetTwoPayOnePrice { pricePerUnit: 5.00 },
+    "TSHIRT": BulkDiscountPrice { pricePerUnit: 20.00, discountPricePerUnit: 19.00, bulkMinUnits: 3 },
+    "MUG": DefaultPrice { pricePerUnit: 7.50 },
+  }
+}
+
+type Checkout struct {
+  prices PricingRules
+  scanChan chan string
+  totalChan chan chan float64
+}
+
+func CheckoutProcess(prices PricingRules,
+                     scanChan chan string,
+                     totalChan chan chan float64) {
+  cart := make(map[string]int)
+  for {
+    select {
+    case s := <- scanChan:
+      count, present := cart[s]
+      if present {
+        cart[s] = count + 1
+      } else {
+        cart[s] = 1
+      }
+    case t := <- totalChan:
+      var total = 0.0
+      for k, v := range cart {
+        total += prices[k].Calculate(v)
+      }
+      t <- total
+    }
+  }
+}
+
+func NewCheckout(prices PricingRules) *Checkout {
+  co := new(Checkout)
+  co.prices = prices
+  co.scanChan = make(chan string)
+  co.totalChan = make(chan chan float64)
+  go CheckoutProcess(prices, co.scanChan, co.totalChan)
+  return co
+}
+
+func (co Checkout) Scan(productName string) {
+  co.scanChan <- productName
+}
+
+func (co Checkout) Total() float64 {
+  rep := make (chan float64)
+  co.totalChan <- rep
+  return <- rep
+}
---
+++ go/checkout_test.go
@@ -0,0 +1,44 @@
+package cabify_challenge
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestScenario1(t *testing.T) {
+  co := NewCheckout(DefaultPrices())
+  co.Scan("VOUCHER")
+  co.Scan("TSHIRT")
+  co.Scan("MUG")
+  assert.Equal(t, 32.50, co.Total())
+}
+
+func TestScenario2(t *testing.T) {
+  co := NewCheckout(DefaultPrices())
+  co.Scan("VOUCHER")
+  co.Scan("TSHIRT")
+  co.Scan("VOUCHER")
+  assert.Equal(t, 25.00, co.Total())
+}
+
+func TestScenario3(t *testing.T) {
+  co := NewCheckout(DefaultPrices())
+  co.Scan("TSHIRT")
+  co.Scan("TSHIRT")
+  co.Scan("TSHIRT")
+  co.Scan("VOUCHER")
+  co.Scan("TSHIRT")
+  assert.Equal(t, 81.00, co.Total())
+}
+
+func TestScenario4(t *testing.T) {
+  co := NewCheckout(DefaultPrices())
+  co.Scan("VOUCHER")
+  co.Scan("TSHIRT")
+  co.Scan("VOUCHER")
+  co.Scan("VOUCHER")
+  co.Scan("MUG")
+  co.Scan("TSHIRT")
+  co.Scan("TSHIRT")
+  assert.Equal(t, 74.50, co.Total())
+}
