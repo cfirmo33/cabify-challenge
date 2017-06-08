3d8adc6ed3f0ff3a499d06797532e308085ef727
Alvaro Polo <apoloval@gmail.com>
2017-06-08 09:22:23 +0200

    [Go] Introduce the `GetTwoPayOnePrice`

--- go/pricing.go
+++ go/pricing.go
@@ -19,3 +19,19 @@ func NewDefaultPrice(p float64) *DefaultPrice {
 func (p DefaultPrice) Calculate(units int) float64 {
   return p.pricePerUnit * float64(units)
 }
+
+// A price policy that applies a discount of 2-for-1.
+type GetTwoPayOnePrice struct {
+  pricePerUnit float64
+}
+
+func NewGetTwoPayOnePrice(p float64) *GetTwoPayOnePrice {
+  dp := new(GetTwoPayOnePrice)
+  dp.pricePerUnit = p
+  return dp
+}
+
+func (p GetTwoPayOnePrice) Calculate(units int) float64 {
+  n := units - (units / 2)
+  return p.pricePerUnit * float64(n)
+}
--- go/pricing_test.go
+++ go/pricing_test.go
@@ -12,3 +12,12 @@ func TestDefaultPrice(t *testing.T) {
   assert.Equal(t, 1.95, p.Calculate(1))
   assert.Equal(t, 3.90, p.Calculate(2))
 }
+
+func TestGetTwoPayOnePrice(t *testing.T) {
+  p := NewGetTwoPayOnePrice(1.95)
+  assert.Equal(t, 0.0, p.Calculate(0))
+  assert.Equal(t, 1.95, p.Calculate(1))
+  assert.Equal(t, 1.95, p.Calculate(2))
+  assert.Equal(t, 3.90, p.Calculate(3))
+  assert.Equal(t, 3.90, p.Calculate(4))
+}
