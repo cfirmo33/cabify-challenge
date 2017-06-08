bf10dfec1cbaf9f40a232608dd3fd28dcbe465c8
Alvaro Polo <apoloval@gmail.com>
2017-06-08 19:21:42 +0200

    [Rust] Implement `GetTwoPayOne`

--- rust/src/pricing.rs
+++ rust/src/pricing.rs
@@ -16,6 +16,18 @@ impl Pricing for DefaultPrice {
     }
 }
 
+// A pricing policy get two and pay one discount.
+pub struct GetTwoPayOne {
+    price_per_units: f32
+}
+
+impl Pricing for GetTwoPayOne {
+    fn calculate(&self, units: u32) -> f32 {
+        (units - units / 2) as f32 * self.price_per_units
+    }
+}
+
+
 #[cfg(test)]
 mod test {
 
@@ -28,5 +40,16 @@ mod test {
         assert_eq!(0.00, p.calculate(0));
         assert_eq!(1.95, p.calculate(1));
         assert_eq!(3.90, p.calculate(2));
+    }
+
+    #[test]
+    fn test_get_two_pay_one() {
+        let p = GetTwoPayOne { price_per_units: 1.95 };
+
+        assert_eq!(0.00, p.calculate(0));
+        assert_eq!(1.95, p.calculate(1));
+        assert_eq!(1.95, p.calculate(2));
+        assert_eq!(3.90, p.calculate(3));
+        assert_eq!(3.90, p.calculate(4));
     }
 }
