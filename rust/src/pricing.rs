af4fccc255acabee225f3f6f7cb1245ad0dbe4fd
Alvaro Polo <apoloval@gmail.com>
2017-06-08 19:25:05 +0200

    Implement `BulkDiscount`

--- rust/src/pricing.rs
+++ rust/src/pricing.rs
@@ -26,6 +26,22 @@ impl Pricing for GetTwoPayOne {
         (units - units / 2) as f32 * self.price_per_units
     }
 }
+// A pricing policy bulk discount
+pub struct BulkDiscount {
+    price_per_units: f32,
+    discount_price_per_units: f32,
+    min_bulk_units: u32,
+}
+
+impl Pricing for BulkDiscount {
+    fn calculate(&self, units: u32) -> f32 {
+        if units < self.min_bulk_units {
+            units as f32 * self.price_per_units
+        } else {
+            units as f32 * self.discount_price_per_units
+        }
+    }
+}
 
 
 #[cfg(test)]
@@ -51,5 +67,20 @@ mod test {
         assert_eq!(1.95, p.calculate(2));
         assert_eq!(3.90, p.calculate(3));
         assert_eq!(3.90, p.calculate(4));
+    }
+
+    #[test]
+    fn test_bulk_discount() {
+        let p = BulkDiscount {
+            price_per_units: 1.95,
+            discount_price_per_units: 1.75,
+            min_bulk_units: 3
+        };
+
+        assert_eq!(0.00, p.calculate(0));
+        assert_eq!(1.95, p.calculate(1));
+        assert_eq!(3.90, p.calculate(2));
+        assert_eq!(5.25, p.calculate(3));
+        assert_eq!(7.00, p.calculate(4));
     }
 }
