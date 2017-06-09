b41a36e4751ee1e2250fc6078fd15df7d6fe62a6
Alvaro Polo <apoloval@gmail.com>
2017-06-09 18:04:01 +0200

    [Elixir] Implement `Pricing.GetTwoPayOne`

--- elixir/lib/cabify_challenge/pricing.ex
+++ elixir/lib/cabify_challenge/pricing.ex
@@ -11,4 +11,12 @@ defmodule Pricing do
   defimpl Calculator, for: DefaultPrice do
     def calculate(price, units), do: price.price_per_unit * units
   end
+
+  defmodule GetTwoPayOne do
+    defstruct price_per_unit: 0
+  end
+
+  defimpl Calculator, for: GetTwoPayOne do
+    def calculate(price, units), do: price.price_per_unit * (units - div(units, 2))
+  end
 end
--- elixir/test/cabify_challenge/pricing_test.exs
+++ elixir/test/cabify_challenge/pricing_test.exs
@@ -9,5 +9,15 @@ defmodule PricingTest do
     assert Pricing.Calculator.calculate(price, 1) == 1.95
     assert Pricing.Calculator.calculate(price, 2) == 3.90
   end
+
+  test "get-two-pay-one price" do
+    price = %Pricing.GetTwoPayOne{ price_per_unit: 1.95 }
+
+    assert Pricing.Calculator.calculate(price, 0) == 0
+    assert Pricing.Calculator.calculate(price, 1) == 1.95
+    assert Pricing.Calculator.calculate(price, 2) == 1.95
+    assert Pricing.Calculator.calculate(price, 3) == 3.90
+    assert Pricing.Calculator.calculate(price, 4) == 3.90
+  end
   
 end
