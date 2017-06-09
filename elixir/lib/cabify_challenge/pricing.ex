25cc02ec35da9fec817d66ff58dc58ae23adb3d8
Alvaro Polo <apoloval@gmail.com>
2017-06-09 18:09:19 +0200

    [Elixir] Implement `Pricing.BulkDiscount`

--- elixir/lib/cabify_challenge/pricing.ex
+++ elixir/lib/cabify_challenge/pricing.ex
@@ -19,4 +19,20 @@ defmodule Pricing do
   defimpl Calculator, for: GetTwoPayOne do
     def calculate(price, units), do: price.price_per_unit * (units - div(units, 2))
   end
+
+  defmodule BulkDiscount do
+    defstruct price_per_unit: 0,
+              discount_price_per_unit: 0,
+              min_bulk_units: 0
+  end
+
+  defimpl Calculator, for: BulkDiscount do
+    def calculate(price, units) do
+      if units < price.min_bulk_units do
+        units * price.price_per_unit
+      else
+        units * price.discount_price_per_unit
+      end
+    end
+  end
 end
--- elixir/test/cabify_challenge/pricing_test.exs
+++ elixir/test/cabify_challenge/pricing_test.exs
@@ -19,5 +19,19 @@ defmodule PricingTest do
     assert Pricing.Calculator.calculate(price, 3) == 3.90
     assert Pricing.Calculator.calculate(price, 4) == 3.90
   end
+
+  test "bulk discount price" do
+    price = %Pricing.BulkDiscount {
+      price_per_unit: 1.95,
+      discount_price_per_unit: 1.75,
+      min_bulk_units: 3
+    }
+
+    assert Pricing.Calculator.calculate(price, 0) == 0
+    assert Pricing.Calculator.calculate(price, 1) == 1.95
+    assert Pricing.Calculator.calculate(price, 2) == 3.90
+    assert Pricing.Calculator.calculate(price, 3) == 5.25
+    assert Pricing.Calculator.calculate(price, 4) == 7.00
+  end
   
 end
