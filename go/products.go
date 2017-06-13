3385427b79d4a821c7cf61829aff09a265835fe4
Alvaro Polo <apoloval@gmail.com>
2017-06-13 18:43:36 +0200

    [Go] Abstract products and catalog in `products.go`

---
+++ go/products.go
@@ -0,0 +1,72 @@
+package cabify_challenge
+
+import (
+	"fmt"
+)
+
+// A product that can be sell in our online store
+type Product struct {
+	Code  string
+	Name  string
+	Price PriceCalculator
+}
+
+// A convenience function to calculate price for the given units
+func (p Product) CalculatePrice(units int) float64 {
+	return p.Price.Calculate(units)
+}
+
+// A catalog of the products available in the online store
+type Catalog struct {
+	Content map[string]Product
+}
+
+// Create a new fresh and empty catalog
+func NewCatalog() *Catalog {
+	cat := new(Catalog)
+	cat.Content = make(map[string]Product)
+	return cat
+}
+
+func (c Catalog) IsEmpty() bool {
+	return len(c.Content) == 0
+}
+
+// Add a new element to the catalog
+func (c Catalog) AddProduct(p Product) {
+	c.Content[p.Code] = p
+}
+
+// Get a product by its code, or return error if not found
+func (c Catalog) GetProduct(code string) (*Product, error) {
+	prod, ok := c.Content[code]
+	if ok {
+		return &prod, nil
+	} else {
+		return nil, fmt.Errorf("no product found for such code %s", code)
+	}
+}
+
+// The default catalog of our online store
+func DefaultCatalog() *Catalog {
+	cat := NewCatalog()
+	cat.AddProduct(Product{
+		"VOUCHER",
+		"Cabify Voucher",
+		GetTwoPayOnePrice{PricePerUnit: 5.00},
+	})
+	cat.AddProduct(Product{
+		"TSHIRT",
+		"Cabify T-Shirt",
+		BulkDiscountPrice{
+			PricePerUnit:         20.00,
+			DiscountPricePerUnit: 19.00,
+			BulkMinUnits:         3},
+	})
+	cat.AddProduct(Product{
+		"MUG",
+		"Cafify Coffee Mug",
+		DefaultPrice{PricePerUnit: 7.50},
+	})
+	return cat
+}
---
+++ go/products_test.go
@@ -0,0 +1,53 @@
+package cabify_challenge
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewCatalogIsEmpty(t *testing.T) {
+	cat := NewCatalog()
+
+	assert.True(t, cat.IsEmpty())
+}
+
+func TestAddAndGetCatalogProducts(t *testing.T) {
+	prod1 := Product{"XX", "description", DefaultPrice{PricePerUnit: 1.00}}
+	prod2 := Product{"YY", "description", DefaultPrice{PricePerUnit: 2.00}}
+	prod3 := Product{"ZZ", "description", DefaultPrice{PricePerUnit: 3.00}}
+	cat := NewCatalog()
+	cat.AddProduct(prod1)
+	cat.AddProduct(prod2)
+	cat.AddProduct(prod3)
+
+	rep1, _ := cat.GetProduct("XX")
+	assert.Equal(t, prod1, *rep1)
+	rep2, _ := cat.GetProduct("YY")
+	assert.Equal(t, prod2, *rep2)
+	rep3, _ := cat.GetProduct("ZZ")
+	assert.Equal(t, prod3, *rep3)
+}
+
+func TestAddProductsToCatalogOverwriteExisting(t *testing.T) {
+	prod1 := Product{"XX", "description", DefaultPrice{PricePerUnit: 1.00}}
+	prod2 := Product{"XX", "other description", DefaultPrice{PricePerUnit: 2.00}}
+	cat := NewCatalog()
+	cat.AddProduct(prod1)
+	cat.AddProduct(prod2)
+
+	rep, _ := cat.GetProduct("XX")
+	assert.Equal(t, prod2, *rep)
+}
+
+func TestGetProductFailsIfNotPresent(t *testing.T) {
+	cat := NewCatalog()
+	_, err := cat.GetProduct("XX")
+	assert.EqualError(t, err, "no product found for such code XX")
+}
+
+func TestDefaultCatalogIsNotEmpty(t *testing.T) {
+	cat := DefaultCatalog()
+
+	assert.False(t, cat.IsEmpty())
+}
+
