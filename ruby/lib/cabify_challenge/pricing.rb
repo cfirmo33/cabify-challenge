0e492c41036513cbd0e4fd99ce84726b8d027ad4
Alvaro Polo <apoloval@gmail.com>
2017-06-13 20:28:19 +0200

    [Ruby] Add some methods to init prices from hashes

--- ruby/lib/cabify_challenge/pricing.rb
+++ ruby/lib/cabify_challenge/pricing.rb
@@ -1,8 +1,18 @@
 module CabifyChallenge
   module Pricing
 
+    def self.from_hash(hash)
+      case hash[:discount]
+        when NilClass then DefaultPrice.new(hash[:price])
+        when "2x1" then GetTwoPayOnePrice.new(hash[:price])
+        when "bulk" then BulkDiscountPrice.new(hash[:price], hash[:bulk_price], hash[:bulk_units])
+      end
+    end
+
     # A price policy that applies no discount at all.
     class DefaultPrice
+
+      attr_reader :price_per_unit
 
       # Initialize this default price with the given price per unit.
       #
@@ -23,6 +33,8 @@ module CabifyChallenge
     # A price policy that applies a discount of 2-for-1.
     class GetTwoPayOnePrice
 
+      attr_reader :price_per_unit
+
       # Initialize this price with the given price per unit.
       #
       # @param price_per_unit [Float]
@@ -41,6 +53,10 @@ module CabifyChallenge
 
     # A price policy that applies a discount for bulk purchases.
     class BulkDiscountPrice
+
+      attr_reader :price_per_unit
+      attr_reader :discount_price_per_unit
+      attr_reader :bulk_minimum_units
 
       # Initialize this price with the given price per unit.
       #
--- ruby/test/cabify_challenge/pricing_test.rb
+++ ruby/test/cabify_challenge/pricing_test.rb
@@ -4,6 +4,26 @@ require 'cabify_challenge'
 module CabifyChallenge
   class PricingTest < Minitest::Test
 
+    def test_init_default_from_hash
+      price = Pricing.from_hash(price: 1.95)
+      assert price.is_a?(Pricing::DefaultPrice)
+      assert_equal 1.95, price.price_per_unit
+    end
+
+    def test_init_get_two_pay_one_from_hash
+      price = Pricing.from_hash(price: 1.95, discount: "2x1")
+      assert price.is_a?(Pricing::GetTwoPayOnePrice)
+      assert_equal 1.95, price.price_per_unit
+    end
+
+    def test_init_bulk_discount_from_hash
+      price = Pricing.from_hash(price: 1.95, discount: "bulk", bulk_price: 1.75, bulk_units: 3)
+      assert price.is_a?(Pricing::BulkDiscountPrice)
+      assert_equal 1.95, price.price_per_unit
+      assert_equal 1.75, price.discount_price_per_unit
+      assert_equal 3, price.bulk_minimum_units
+    end
+
     def test_default_price
       price = Pricing::DefaultPrice.new(1.95)
 
